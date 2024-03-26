a9c5d3090d002faea2a67259170fd5babb0206d1
Srinjoy Sengupta <srinjoy003@gmail.com>
2024-03-26 07:19:35 +0530

    Test for deployment

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 
 const mongoURI = process.env.MONGODB_URI as string;
-mongoose.connect(mongoURI);
+// mongoose.connect(mongoURI);
 
 process.on("SIGINT", async () => {
 	await mongoose.connection.close();
@@ -16,62 +16,38 @@ function generateSessionId() {
 	return uuidv4();
 }
 
-// export async function POST(request: NextRequest) {
-// 	try {
-// 		const requestBody = await request.json();
-
-// 		const user = await Users.findOne({ username: requestBody.username });
-
-// 		if (!user) {
-// 			return new NextResponse("Account with this username does not exist", {
-// 				status: 404,
-// 			});
-// 		}
-
-// 		const passwordMatch = await bcrypt.compare(
-// 			requestBody.password,
-// 			user.password
-// 		);
-
-// 		if (!passwordMatch) {
-// 			return new NextResponse("Username and password do not match", {
-// 				status: 401,
-// 			});
-// 		}
-
-// 		const sessionId = generateSessionId();
-
-// 		return new NextResponse(
-// 			JSON.stringify({
-// 				sessionId,
-// 				message: "Login successful",
-// 				username: requestBody.username,
-// 			}),
-// 			{
-// 				status: 200,
-// 				headers: {
-// 					"Content-Type": "application/json",
-// 				},
-// 			}
-// 		);
-// 	} catch (error) {
-// 		console.error("Error processing login", error);
-
-// 		return new NextResponse("Internal Server Error", {
-// 			status: 500,
-// 		});
-// 	}
-// }
-
 export async function POST(request: NextRequest) {
 	try {
-		
+		mongoose.connect(mongoURI);
+
+		const requestBody = await request.json();
+
+		const user = await Users.findOne({ username: requestBody.username });
+
+		if (!user) {
+			return new NextResponse("Account with this username does not exist", {
+				status: 404,
+			});
+		}
+
+		const passwordMatch = await bcrypt.compare(
+			requestBody.password,
+			user.password
+		);
+
+		if (!passwordMatch) {
+			return new NextResponse("Username and password do not match", {
+				status: 401,
+			});
+		}
+
+		const sessionId = generateSessionId();
 
 		return new NextResponse(
 			JSON.stringify({
-				sessionId:"duabcnbwud",
+				sessionId,
 				message: "Login successful",
-				username: "TestUsername"
+				username: requestBody.username,
 			}),
 			{
 				status: 200,
@@ -88,3 +64,28 @@ export async function POST(request: NextRequest) {
 		});
 	}
 }
+
+// export async function POST(request: NextRequest) {
+// 	try {
+// 		console.log("Api request recieved");
+// 		return new NextResponse(
+// 			JSON.stringify({
+// 				sessionId: "duabcnbwud",
+// 				message: "Login successful",
+// 				username: "TestUsername",
+// 			}),
+// 			{
+// 				status: 200,
+// 				headers: {
+// 					"Content-Type": "application/json",
+// 				},
+// 			}
+// 		);
+// 	} catch (error) {
+// 		console.error("Error processing login", error);
+
+// 		return new NextResponse("Internal Server Error", {
+// 			status: 500,
+// 		});
+// 	}
+// }
